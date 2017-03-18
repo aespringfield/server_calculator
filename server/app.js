851d5edd467c0615ba4635c7d032112b31584597
Anna Springfield <anna.e.springfield@gmail.com>
2017-03-18 13:06:59 -0500

    set up buttons and post

---
+++ server/app.js
@@ -0,0 +1,27 @@
+// add requires and global variables
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+
+app.set("port", 4000);
+
+// uses
+app.use(express.static("server/public"));
+// change this to module?
+app.get("/", function(req, res){
+  res.sendFile(path.resolve("server/public/views/index.html"));
+});
+// change this to module?
+app.post("/calculate/:x/:y/:type", function(req, res){
+  var x = req.params.x;
+  var y = req.params.y;
+  var type = req.params.type;
+  console.log("x is", x, "\ny is", y, "\ntype is", type, "\n");
+  res.sendStatus(200);
+});
+
+// spin up server
+app.listen(app.get("port"), function() {
+  console.log("Listening on port:", app.get("port"));
+});
---
+++ server/public/scripts/client.js
@@ -0,0 +1,73 @@
+// run functions on ready
+$(document).ready(function() {
+  listenForClicks();
+});
+
+
+// event handlers
+function listenForClicks() {
+  setOperator();
+  clearResult();
+  startCalculation();
+}
+
+// attach operator to a data value in equals div when clicked
+function setOperator() {
+  $(".operator").on("click", function() {
+    var operator = $(this).data("operator");
+    console.log(operator);
+    $("#equals").data("currentOperator", operator);
+  });
+}
+
+// when clear div is clicked, empty old result-text when clear div
+function clearResult() {
+  $("#clear").on("click", function() {
+    console.log("clear");
+  });
+}
+
+// when equals div is clicked, create calculation object and send to server
+function startCalculation() {
+  $("#equals").on("click", function() {
+    console.log("equals");
+    // change below to grab from form?
+    var x = retrieveInput("value1");
+    var y = retrieveInput("value2");
+    var type = $(this).data("currentOperator");
+    console.log(type);
+    var calcObject = new CalcObject(x, y, type);
+    console.log(calcObject);
+    postCalcObject(calcObject);
+  });
+}
+
+// retrieve value from input field and convert to number
+function retrieveInput(id) {
+  var inputValue = $("#" + id).val();
+  inputValue = parseInt(inputValue);
+  return inputValue;
+}
+
+// calculation object constructor
+function CalcObject(x, y, type) {
+  this.x = x;
+  this.y = y;
+  this.type = type;
+}
+
+// use ajax post to send object to server
+function postCalcObject(calcObject) {
+  $.ajax({
+    type: "POST",
+    url: "/calculate/" + calcObject.x + "/" + calcObject.y + "/" + calcObject.type,
+    data: calcObject,
+    success: function(response) {
+      console.log("Successful post!");
+    }
+  });
+}
+
+// use ajax get to retrieve calculated result from server
+
+// change result-text to new result
